3f71bdcaee8e2f920209e5ad51ad3fe18b7fb1bc
Chie Fuyuki <cfuyuki@gmail.com>
2015-03-03 17:28:12 +0800

    Version 0.2.0

--- templates/content-single.php
+++ templates/content-single.php
@@ -8,7 +8,10 @@
       <?php the_content(); ?>
     </div>
     <footer>
-      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+      <?php wp_link_pages(array('before' => '<nav class="page-nav aligncenter mrgn-40"><ul class="pagination pagination-lg">'
+      . '<li class="disabled"><span>
+' . __('Pages', 'roots') . '</span></li><li><span>', 'after' => '</span></li></ul></nav>', 'separator' =>
+      '</span></li><li><span>',)); ?>
     </footer>
     <?php if(!is_page())comments_template('/templates/comments.php'); ?>
   </article>
--- templates/header.php
+++ templates/header.php
@@ -10,10 +10,10 @@
 		$logo_x2 = get_option( 'ts_logo_x2' );
 
 		if ( $logo == '' ) {
-			$logo = get_template_directory_uri() . '/images/logo_tools.png';
+			$logo = get_template_directory_uri() . '/assets/img/logo_synsth-s.png';
 		}
 		if ( $logo_x2 == '' ) {
-			$logo_x2 = $logo;
+			$logo_x2 = get_template_directory_uri() . '/assets/img/logo_synsth-s.svg';
 		}
 	?>
 		<img class="default" src="<?php echo $logo; ?>" alt="<?php bloginfo( 'name' ); ?>" />
@@ -22,16 +22,26 @@
     
     <!--<a id="menu" class="" href="#"><i class="fa fa-reorder"></i></a>-->
     <div id="menu">
-    <?php echo do_shortcode('
+    <?php
+
+	$menu = wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav', 'echo' =>
+		false, 'fallback_cb' => '__return_false'));//
+	if (!$menu) $menu =
+		'<div class="alert alert-info skiny-centered">'
+		. '<h6>' . __('Menu is empty!', 'root') . '</h6>'
+		. __('Create a menu and assign it to "Primary Navigation" on dashboard "Appearance/Menus"',
+							  'root') . '</div>';
+
+	echo do_shortcode('
     	[ts_modal id="menu" type="fullscreen"]
     	[ts_modal_button]
-    	[bs_button style="btn btn-menu tmpl-line" icon="glyphicon glyphicon-menu-hamburger" type="button" title=""]
+    	<button class="btn btn btn-menu tmpl-line"><i class="glyphicon glyphicon-menu-hamburger"></i></button>
     	[/ts_modal_button]
     	[ts_modal_header]'
     	. '<h1 style="height:100px">' . '<img class="default aligncenter" src="' . $logo . '" />' . '</h1>'
     	.'[/ts_modal_header]
     	[ts_modal_content]'
-    	. wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav', 'echo' => false))
+    	. $menu
     	.'[/ts_modal_content]
     	[/ts_modal]');
     ?>
